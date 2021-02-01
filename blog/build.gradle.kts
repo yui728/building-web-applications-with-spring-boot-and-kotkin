70df23652342f9dc14adbb6f19aeff955ebf67ed
yui728 <yui.nishimra.1984@gmail.com>
2021-02-01 21:12:25 +0900

    Complete Section:Exposing HTTP API

--- blog/build.gradle.kts
+++ blog/build.gradle.kts
@@ -6,6 +6,7 @@ plugins {
 	kotlin("jvm") version "1.4.21"
 	kotlin("plugin.spring") version "1.4.21"
 	kotlin("plugin.jpa") version "1.4.21"
+	kotlin("kapt") version "1.4.21"
 }
 
 allOpen {
@@ -16,7 +17,7 @@ allOpen {
 
 group = "com.example"
 version = "0.0.1-SNAPSHOT"
-java.sourceCompatibility = JavaVersion.VERSION_11
+java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 repositories {
 	mavenCentral()
@@ -31,7 +32,16 @@ dependencies {
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
 	developmentOnly("org.springframework.boot:spring-boot-devtools")
 	runtimeOnly("com.h2database:h2")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	kapt("org.springframework.boot:spring-boot-configuration-processor")
+//	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+		exclude(module = "mockito-core")
+	}
+
+	testImplementation("org.junit.jupiter:junit-jupiter-api")
+	testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+	testImplementation("com.ninja-squad:springmockk:3.0.1")
 }
 
 tasks.withType<KotlinCompile> {
--- blog/src/main/kotlin/com/example/blog/HtmlContollers.kt
+++ blog/src/main/kotlin/com/example/blog/HtmlContollers.kt
@@ -6,6 +6,8 @@ import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.server.ResponseStatusException
 
 @Controller
@@ -46,4 +48,26 @@ class HtmlContrßoller(private val repository: ArticleRepository){
         val author: User,
         val addedAt: String
     )
+}
+
+@RestController
+@RequestMapping("/api/article")
+class ArticleController(private val repository: ArticleRepository) {
+    @GetMapping("/")
+    fun findAll() = repository.findAllByOrderByAddedAtDesc()
+
+    @GetMapping("/[slug}")
+    fun findOne(@PathVariable slug: String) =
+        repository.findBySlug(slug) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This article does not exist")
+}
+
+@RestController
+@RequestMapping("/api/user")
+class UserController(private val repository: UserRepository) {
+    @GetMapping("/")
+    fun findAll() = repository.findAll()
+
+    @GetMapping("/{login}")
+    fun findOne(@PathVariable login: String) =
+        repository.findByLogin(login) ?: throw ResponseStatusException(HttpStatus.NOT_FOUND, "This user does not exist")
 }
